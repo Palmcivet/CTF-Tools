dd58be2d36e64638464dcee7c483b60306bf8eb7
Palm Civet <palmcivet_email@qq.com>
2020-01-12 03:32:57 +0800

    :sparkle: design SideTree.
    write a function for retrieve.

--- src/Components/SideTree.js
+++ src/Components/SideTree.js
@@ -1,17 +1,97 @@
+import { urlencode } from '../Libs/urlencode';
+import { quotedPrintable } from '../Libs/quotedPrintable';
+import { base64 } from '../Libs/essential/base64';
 
-const SideTree = {
-    "Base": [
-        "Base 36",
-        "Base 58",
-        "Base 62",
-        "Base 85",
-        "Base 91",
-        "Base 92",
-    ],
-    "Encode": [
-        "Unicode",
-        "URLencode",
-    ]
+const BASE = [
+    "Base",
+    "Base 家族编码",
+    {
+        base36: {
+            TAG: "Base 36",
+            LIB: "",
+            COMMENT: "",
+        },
+        base64: {
+            TAG: "Base 64",
+            LIB: base64,
+            COMMENT: "",
+        },
+        base58: {
+            TAG: "Base 58",
+            LIB: "",
+            COMMENT: "",
+        },
+        base62: {
+            TAG: "Base 62",
+            LIB: "",
+            COMMENT: "",
+        },
+        base85: {
+            TAG: "Base 85",
+            LIB: "",
+            COMMENT: "",
+        },
+        base91: {
+            TAG: "Base 91",
+            LIB: "",
+            COMMENT: "",
+        },
+        base92: {
+            TAG: "Base 92",
+            LIB: "",
+            COMMENT: "",
+        },
+    }
+];
+
+const TYPICAL = [
+    "typical",
+    "常见编码",
+    {
+        unicode: {
+            TAG: "Unicode",
+            LIB: unicode,
+            COMMENT: "",
+        },
+        urlencode: {
+            TAG: "URLencode",
+            LIB: urlencode,
+            COMMENT: ""
+        },
+        quotedprintable: {
+            TAG: "Quoted-Printable",
+            LIB: quotedPrintable,
+            COMMENT: ""
+        },
+    }
+];
+
+const ROT = [
+    "ROT",
+    "",
+    {
+        quotedprintable: {
+            TAG: "Quoted-Printable",
+            LIB: quotedPrintable,
+            COMMENT: ""
+        },
+    }
+];
+
+const SideTree = [TYPICAL, BASE, ROT,];
+
+/**
+ * 传入函数名，返回函数
+ * @param {String} argLibName - 待检索的函数名
+ * @returns 返回函数
+ */
+const retrieve = (argLibName) => {
+    for (let value of SideTree) {
+        let val = value[2];
+        if (val.hasOwnProperty(argLibName)) {
+            return val[argLibName].LIB;;
+        }
+    }
 };
 
-export { SideTree };
+export { SideTree, retrieve };
